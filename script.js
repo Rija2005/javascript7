989af5e13d168fb473f001b76a01a11ce6d8aff6
Rija2005 <164767074+Rija2005@users.noreply.github.com>
2024-06-03 15:56:47 +0500

    my practice

---
+++ script.js
@@ -0,0 +1,99 @@
+//loops;to write one thing in continuty
+// for(let i = 0;i < 20;i++){
+//   document.write("Hello world")
+// }
+
+// for(let i = 14; i < 141; i = i+14){
+// console.log(i);
+// }
+// for(let i =24 ; i<241 ; i=i+24){
+//     console.log(i);
+// }
+// let cleanestCities=["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// let cityToCheck="Great Falls"
+
+// for (let i = 0; i <= 4; i++) {
+//      if (cityToCheck === cleanestCities[3]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else{
+//         alert("It's  not one of the cleanest cities")
+//      }
+//     }
+
+
+
+
+// let months=["janurary","feburary,","march","April","May","June"];
+// let monthTocheck="June";
+
+// for (let i = 0; i <= 4; i++) {
+//     if(monthTocheck===months[5]){
+//         alert("its my Birthday Month")
+//     }
+//     else if (monthTocheck===months[2]){
+//         alert("its not my birthday months")
+//     }
+//     else{
+//         alert("its very cold")
+//     }
+// }
+
+// let qualificationOfpak=["SSC","HSC","BCS","BS","BCOM","MS","M.PHILL","PHD"];
+// document.write("<h3>Qualifications</h3>");
+// document.write("<ol>");
+// for(let i =0;i<=7;i++){
+//     document.write("<li>"+qualificationOfpak[i]+"</li>");
+
+// }
+// document.write("</ol>");
+
+
+// let fruitsOfpak=["Mango","Orange","Banana","Melon","Berry","Grapes","Lichee","Papya"];
+// document.write("<h3>FRUITS</h3>");
+// document.write("<ul>");
+// for(let i =0;i<=7;i++){
+//     document.write("<li>"+fruitsOfpak[i]+"</li>");
+
+// }
+// document.write("</ul>");
+
+
+// let color=["Red","Green","Pink","Purple","Yellow","Black","White"];
+// document.write("My orginal array is:"+color);
+// let color_to_add=prompt("Enter the color of your Choice!");
+// color.push(color_to_add);
+// document.write(" <br>My unshift is"+color);
+
+
+// let color=["Red","Green","Pink","Purple","Yellow","Black","White"];
+// document.write("My orginal array is:"+color);
+
+// color.push("black, brown");
+// document.write(" <br>My unshift is"+color);
+
+
+
+// let color=["Red","Green","Pink","Purple","Yellow","Black","White"];
+// document.write("My orginal array is:"+color);
+// let color_to_add=prompt("Enter the color of your Choice!");
+// color.pop(color_to_add);
+// document.write(" <br>My unshift is"+color);
+
+
+
+// let color=["Red","Green","Pink","Purple","Yellow","Black","White"];
+// document.write("My orginal array is:"+color);
+// let index_to_add=prompt("At which index color will be add")
+// let color_to_add=prompt("Enter the color of your Choice!");
+// color.splice(index_to_add,0,color_to_add);
+// document.write(" <br>My unshift is"+color);
+
+
+
+// let color=["Red","Green","Pink","Purple","Yellow","Black","White"];
+// document.write("My orginal array is:"+color);
+
+// let color_to_add=prompt("Enter the color of your Choice!");
+// color.push(color_to_add);
+// document.write(" <br>My unshift is"+color);
